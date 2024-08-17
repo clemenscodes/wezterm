0c9492bafd08a0e98e52f560a830058bfdf723f3
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-17 13:57:01 +0200

    chore: use gl backend

--- config/appearance.lua
+++ config/appearance.lua
@@ -1,3 +1,6 @@
+local wezterm = require('wezterm')
+local gpus = wezterm.gui.enumerate_gpus()
+
 return {
    color_scheme = "catppuccin-macchiato",
    enable_tab_bar = false,
@@ -6,7 +9,7 @@ return {
    audible_bell = "Disabled",
    window_close_confirmation = 'NeverPrompt',
    win32_system_backdrop = 'Acrylic', -- Does not work with WebGpu frontend
-   window_background_opacity = 0.9,
+   window_background_opacity = 0.8,
    window_padding = {
      left = 0,
      right = 0,
@@ -14,4 +17,6 @@ return {
      bottom = 0,
    },
    front_end = 'WebGpu',
+   webgpu_power_preference = "HighPerformance",
+   webgpu_preferred_adapter = gpus[4],
 }
